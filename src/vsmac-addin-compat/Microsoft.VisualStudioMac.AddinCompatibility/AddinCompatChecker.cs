80b93b254011e11f40791e88d4770bcc470ee10e
Matt Ward <matt.ward@microsoft.com>
2022-11-13 17:19:26 +0000

    Disable embedded interop and version mismatch reporting

--- src/vsmac-addin-compat/Microsoft.VisualStudioMac.AddinCompatibility/AddinCompatChecker.cs
+++ src/vsmac-addin-compat/Microsoft.VisualStudioMac.AddinCompatibility/AddinCompatChecker.cs
@@ -52,6 +52,8 @@ class AddinCompatChecker : IDisposable
 
         Checker.ReportIntPtrConstructors = true;
         Checker.ReportVersionMismatch = false;
+        Checker.ReportEmbeddedInteropTypes = false;
+        Checker.ReportVersionMismatch = false;
 
         var checker = new Checker();
 
