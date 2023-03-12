786c05cd023b9ade4c2488054f1092effd6dfdeb
Matt Ward <matt.ward@microsoft.com>
2023-03-12 17:16:47 +0000

    Update addin version to 0.2

--- src/ExtensionCompatibilityChecker/Properties/AddinInfo.cs
+++ src/ExtensionCompatibilityChecker/Properties/AddinInfo.cs
@@ -29,7 +29,7 @@ using Mono.Addins;
 
 [assembly:Addin("ExtensionCompatibilityChecker",
                 Namespace = "Microsoft.VisualStudioMac",
-                Version = "0.1",
+                Version = "0.2",
                 Category = "IDE extensions")]
 
 [assembly:AddinName("Extension Compatibility Checker")]
